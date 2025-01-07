a6e7f21c9ed95a8671c239292d72bc6b127c6d17
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-01-06 22:21:25 -0300

    Feat: Create postgresql

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilitiesFor } from '@saas/auth'
-
-const ability = defineAbilitiesFor({ role: 'ADMIN' })
-
-const userCanInviteSomeneElse = ability.can('get', 'projects')
-
-console.log(userCanInviteSomeneElse)
