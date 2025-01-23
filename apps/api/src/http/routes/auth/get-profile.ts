28e060fe2b7a114c6fbfe1ed8aedc1c4ef567bf9
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-01-22 23:10:47 -0300

    Feat: Create route get-profile

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,48 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z, { string } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authemticated user profile',
+        response: {
+          200: z.object({
+            user: z.object({
+              id: string(),
+              name: string().nullable(),
+              email: string(),
+              avatarUrl: string().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+
+      if (!user) {
+        throw new Error('User not found')
+      }
+
+      return reply.send({ user })
+    },
+  )
+}
