2be502ca91b153e24a7ac22ed6e323e8571f450b
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-02-02 00:09:16 -0300

    Feat: create route delete members

---
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -0,0 +1,54 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { UnanuthorizedError } from '@/http/_erros/unanuthorized-error'
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/http/utils/get-user-permissions'
+import { prisma } from '@/lib/prisma'
+
+export async function removeMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:orgSlug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'remove a member from organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            orgSlug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          response: {
+            2024: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { orgSlug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'User')) {
+          throw new UnanuthorizedError(
+            `You're not allowed to remove this members.`,
+          )
+        }
+
+        await prisma.member.delete({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
