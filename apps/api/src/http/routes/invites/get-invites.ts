652840dfee60d421c90215f83e0df92f42d7686c
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-02-03 09:42:41 -0300

    Feat: create route get invites

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,86 @@
+import { rolesSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { UnanuthorizedError } from '@/http/_erros/unanuthorized-error'
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/http/utils/get-user-permissions'
+import { prisma } from '@/lib/prisma'
+
+export async function getInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'get all organization invites',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            email: z.string().email(),
+            role: rolesSchema,
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              invites: z
+                .array(
+                  z.object({
+                    id: z.string(),
+                    createdAt: z.date(),
+                    role: rolesSchema,
+                    email: z.string(),
+                    author: z
+                      .object({
+                        name: z.string().nullable(),
+                        id: z.string().uuid(),
+                      })
+                      .nullable(),
+                  }),
+                )
+                .nullable(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnanuthorizedError(`You're not allowed to read invites.`)
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+        return reply.status(200).send({ invites })
+      },
+    )
+}
