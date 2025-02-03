67bd60a22a16b349c74300641c791de00c58a545
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-02-03 13:29:30 -0300

    Feat: crete route billing

---
+++ apps/api/src/http/routes/billing/get-organization-billing.ts
@@ -0,0 +1,88 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/http/utils/get-user-permissions'
+import { prisma } from '@/lib/prisma'
+
+export async function getOrganizationBilling(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get billing information from organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              total: z.number(),
+              billing: z.object({
+                seats: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+                projects: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Billing')) {
+          throw new Error(
+            `you're not allowed to get the billing details from this organization.`,
+          )
+        }
+
+        const [amountOfMembers, amountOfProjects] = await Promise.all([
+          prisma.member.count({
+            where: {
+              organizationId: organization.id,
+              role: { not: 'BILLING' },
+            },
+          }),
+
+          prisma.member.count({
+            where: {
+              organizationId: organization.id,
+            },
+          }),
+        ])
+
+        return reply.status(200).send({
+          total: amountOfMembers * 10 + amountOfProjects * 20,
+          billing: {
+            seats: {
+              amount: amountOfMembers,
+              unit: 10,
+              price: amountOfMembers * 10,
+            },
+            projects: {
+              amount: amountOfProjects,
+              unit: 20,
+              price: amountOfProjects * 20,
+            },
+          },
+        })
+      },
+    )
+}
