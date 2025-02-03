7f0f4aa603ff39b91a766f68d8ec88bc155b0b00
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-02-03 11:29:56 -0300

    Feat: add route accept invite

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,75 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { BadRequestError } from '@/http/_erros/bad-request-error'
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+export async function acceptInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/accept',
+      {
+        schema: {
+          tags: ['invites'],
+          security: [{ bearerAuth: [] }],
+          summary: 'Accept to an invite',
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { inviteId } = request.params
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found or expired.')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        if (invite.authorId !== user.id) {
+          throw new BadRequestError('You cannot accept this invite.')
+        }
+
+        await prisma.$transaction([
+          prisma.member.create({
+            data: {
+              userId: user.id,
+              organizationId: invite.organizationId,
+              role: invite.role,
+            },
+          }),
+
+          prisma.invite.delete({
+            where: {
+              id: invite.id,
+            },
+          }),
+        ])
+
+        return reply.status(204).send()
+      },
+    )
+}
