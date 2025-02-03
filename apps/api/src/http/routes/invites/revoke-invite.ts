27f701db0e8d36921ccc6986d12f52f648e508b6
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-02-03 11:45:14 -0300

    Feat: create route revokeInvite

---
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -0,0 +1,65 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { BadRequestError } from '@/http/_erros/bad-request-error'
+import { UnanuthorizedError } from '@/http/_erros/unanuthorized-error'
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/http/utils/get-user-permissions'
+import { prisma } from '@/lib/prisma'
+
+export async function revokeInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/inviteId',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'revoke an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, inviteId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'Invite')) {
+          throw new UnanuthorizedError(
+            `You're not allowed to delete an invites.`,
+          )
+        }
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found.')
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
